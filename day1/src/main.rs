4ea148dfe29daedb0bdf8a44954264028701c77b
Tamás Barta <tamas@bartatamas.hu>
2023-01-01 21:43:50 +0100

    Improve day1
    I watched ThePrimeagen's stream of day 1 and found some ways to improve
    my existing solution.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,28 +1,18 @@
-use std::{fs::File, io::Read, u32};
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut input = String::new();
-    File::open("input.txt")?.read_to_string(&mut input)?;
+    let input = include_str!("../input.txt");
 
-    let mut elf_calories = input
+    let mut elf_calories: Vec<_> = input
         .split("\n\n")
-        .map(|elf| {
-            elf.split("\n")
-                .filter_map(|food| -> Option<u32> {
-                    food.parse::<u32>().ok()
-                })
-                .sum::<u32>()
-        })
-        .collect::<Vec<u32>>();
+        .map(|elf| elf.lines().flat_map(str::parse::<usize>).sum())
+        .collect();
+
+    println!("Most calories: {}", elf_calories.iter().max().unwrap_or_else(|| &0));
 
     elf_calories.sort();
-    elf_calories.reverse();
 
-    let (first_3, _) = elf_calories.split_at(3);
+    let max_elf_cals: usize = elf_calories.iter().rev().take(3).sum();
 
-    let max_elf_calories = first_3.iter().sum::<u32>();
-
-    println!("Max calorires: {}", max_elf_calories);
+    println!("Sum of calories of top 3: {}", max_elf_cals);
 
     Ok(())
 }
